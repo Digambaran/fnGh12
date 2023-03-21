fe44a5a9063e8bee81d891acdff6963acf984d7f
Digambaran <arjunsreekumar.1@gmail.com>
2023-03-21 15:49:11 +0530

    fix: comment

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-
-
 const handler = async (event) => {
+  const { req, res } = event
+  // lalsdasdadl
 
-  const {req, res} = event
-  
   // health check
-  if (req.params["health"] === "health") {
-    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+  if (req.params.health === 'health') {
+    res.write(JSON.stringify({ success: true, msg: 'Health check success' }))
     res.end()
   }
 
   // Add your code here
-  res.write(JSON.stringify({success: true, msg: `Happy Hacking`}))
+  res.write(JSON.stringify({ success: true, msg: `Happy Hacking` }))
   res.end()
-  
 }
 
 export default handler
